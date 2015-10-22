97569f3a16174ab3f8e4fee0d11c76428f1c5e5c
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2015-10-23 01:28:01 +0200

    Some updates to the fronted...

--- www/assets/js/controllers.js
+++ www/assets/js/controllers.js
@@ -1,48 +1,97 @@
-var app = angular.module('appTranslator', []);
+var app = angular.module('appTranslator', ['ngFileUpload']);
 
-app.controller('TranslationController', function($scope, $http) {
+app.controller('TranslationController', function ($scope, $http, Upload, $timeout) {
 
-    var url = 'http://127.0.0.1:5000/translate';
+    $scope.url = 'http://127.0.0.1:5050/';
 
     $scope.state = {
-        isLoading : false,
-        loaded : false
-    }
-
-    $scope.data = {
-        string : '',
-        source_lang : '',
-        target_lang : '',
-        translations : [],
-        debug : '',
+        isLoading: false,
+        loaded: false,
+        progress: 0
     };
 
-    $scope.getTranslations = function() {
+    $scope.data = {
+        decoder: {
+            decoder: 'moses',
+            moses: {},
+            solr: {}
+        },
+        config: {
+            source_lang: 'en',
+            target_lang: 'fr',
+            mode: 'string' // string|xml
+        },
+        input: {
+            string: '',
+            file: ''
+        },
+        result: {
+            translations: [],
+            debug: ''
+        }
+    };
+
+    $scope.uploadFile = function (file) {
+        if (file) {
+            $scope.state.progress = 1;
+            file.upload = Upload.upload({
+                url: $scope.url + 'upload',
+                data: {file: file}
+            });
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    console.log(response.data);
+                    if (response.data.success) {
+                        $scope.data.config.mode = 'xml';
+                        $scope.data.input.file = response.data.filename;
+                    } else {
+                        // TODO Error server-side
+                    }
+                });
+            }, function (response) {
+                console.log(response);
+                // TODO Error
+            }, function (evt) {
+                $scope.state.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+                console.log($scope.state.progress);
+                $('#progress').progress('increment', $scope.state.progress);
+            });
+        }
+    };
+
+    $scope.getTranslations = function () {
         $scope.isLoading = true;
         var params = {
-            'from' : $scope.data.source_lang,
-            'to' : $scope.data.target_lang,
-            'string' : $scope.data.string
+            'from': $scope.data.config.source_lang,
+            'to': $scope.data.config.target_lang
         };
-        $http.get(url, {'params':  params}).then(function(response) {
+        var endpoint = '';
+        if ($scope.data.config.mode == 'xml') {
+            endpoint = $scope.url + 'translateXML'
+            params.xml_filename = $scope.data.input.file;
+        } else {
+            endpoint = $scope.url + 'get';
+            params.string = $scope.data.input.string;
+        }
+        $http.get(endpoint, {'params': params}).then(function (response) {
             console.log(response);
-            $scope.data.translations = response.data.translations;
-            $scope.data.debug = JSON.stringify(response.data.debug);
+            $scope.data.result.translations = response.data.translations;
+            $scope.data.result.debug = JSON.stringify(response.data.debug);
             $scope.state.isLoading = false;
             $scope.state.loaded = true;
-        }, function(response) {
+        }, function (response) {
             console.log(response);
             $scope.state.isLoading = false;
         });
     };
 
-    $scope.getLanguages = function() {
+    $scope.getLanguages = function () {
         return [
-            {code : 'en', name: 'English'},
-            {code : 'de', name: 'German'},
-            {code : 'fr', name: 'French'},
-            {code : 'it', name: 'Italian'},
-            {code : 'ru', name: 'Russian'}
+            {code: 'en', name: 'English'},
+            {code: 'de', name: 'German'},
+            {code: 'fr', name: 'French'},
+            {code: 'it', name: 'Italian'},
+            {code: 'ru', name: 'Russian'}
         ];
     };
     $scope.data.languages = $scope.getLanguages();
